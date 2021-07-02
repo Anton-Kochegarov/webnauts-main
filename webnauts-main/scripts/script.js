393e80a69c181dc1e3f4ff33b26ae931f0076d85
Anton-Kochegarov <djam0660@gmail.com>
2021-07-02 16:58:25 +0300

    add 02/07/2021

--- webnauts-main/scripts/script.js
+++ webnauts-main/scripts/script.js
@@ -1,1 +1,31 @@
-console.log('dsgdsg')
+let popUp = (document.querySelector(".pop-up"));
+let popUpBtn = (document.querySelector(".btn"));
+let popUpClose = (document.querySelector(".pop-up-closed"));
+let footerBtn = (document.querySelector(".btn_btn-dark"));
+
+function showPopUpClick() {
+    popUp.classList.add("pop-up-show");
+}
+
+popUpBtn.addEventListener("click", showPopUpClick);
+footerBtn.addEventListener("click", showPopUpClick);
+
+popUpClose.addEventListener("click", function() {
+    popUp.classList.remove("pop-up-show");
+})
+
+
+let swiper = new Swiper(".mySwiper", {
+
+    slidesPerView: 1.18,
+    centeredSlides: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
